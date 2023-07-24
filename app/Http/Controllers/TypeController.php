0c69706cec1a63b1a22be3712518cafffddef674
itsziel <azrilalle@gmail.com>
2023-07-25 06:46:26 +0700

    Crud

--- app/Http/Controllers/TypeController.php
+++ app/Http/Controllers/TypeController.php
@@ -2,9 +2,8 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
 use App\Models\Type;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class TypeController extends Controller
 {
@@ -13,14 +12,9 @@ class TypeController extends Controller
      */
     public function index()
     {
-        // Eloquent
-        $types = Type::all();
-
-        $pageTitle = 'Data Kategori';
-
         return view('admin.type.index', [
-            'pageTitle' => $pageTitle,
-            'types' => $types
+            'pageTitle' => 'Data Kategori',
+            'types' => Type::all()
         ]);
     }
 
@@ -29,15 +23,48 @@ class TypeController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.type.create', [
+            'pageTitle' => 'Tambah Kategori'
+        ]);
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store()
     {
-        //
+        $data = request()->except(['_token']);
+
+        try {
+            $validator = Validator::make(
+                $data,
+                [
+                    'code' => 'required|min:1',
+                    'name' => 'required|min:3'
+                ],
+                [
+                    'required' => ':Attribute harus diisi.',
+                    'min' => ':Attribute terlalu pendek.'
+                ],
+                [
+                    'code' => 'Kode Kategori',
+                    'name' => 'Nama Kategori'
+                ]
+            );
+
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+
+            $category = new Type();
+            $category->kode_tipe = $data['code'];
+            $category->nama_tipe = $data['name'];
+            $category->save();
+
+            return $this->respondRedirectMessage('kategori.index');
+        } catch (\Exception $e) {
+            return "{$e->getMessage()}, {$e->getCode()}";
+        }
     }
 
     /**
@@ -45,7 +72,10 @@ class TypeController extends Controller
      */
     public function show(string $id)
     {
-        //
+        return view('admin.type.show', [
+            'pageTitle' => 'Detail Kategori',
+            'category' => Type::findOrFail($id)
+        ]);
     }
 
     /**
@@ -53,15 +83,50 @@ class TypeController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        return view('admin.type.edit', [
+            'pageTitle' => 'Edit Kategori',
+            'category' => Type::findOrFail($id)
+        ]);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(string $id)
     {
-        //
+        $data = request()->except(['_token']);
+
+        try {
+            $validator = Validator::make(
+                $data,
+                [
+                    'code' => 'required|min:1',
+                    'name' => 'required|min:3'
+                ],
+                [
+                    'required' => ':Attribute harus diisi.',
+                    'min' => ':Attribute terlalu pendek.'
+                ],
+                [
+                    'code' => 'Kode Kategori',
+                    'name' => 'Nama Kategori'
+                ]
+            );
+
+            if ($validator->fails()) {
+                return redirect()->back()->withErrors($validator)->withInput();
+            }
+
+            $category = Type::findOrFail($id);
+            $category->update([
+                'kode_tipe' => $data['code'],
+                'nama_tipe' => $data['name']
+            ]);
+
+            return $this->respondRedirectMessage('kategori.index');
+        } catch (\Exception $e) {
+            return "{$e->getMessage()}, {$e->getCode()}";
+        }
     }
 
     /**
@@ -69,6 +134,13 @@ class TypeController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+        try {
+            $category = Type::findOrFail($id);
+            $category->products()->delete();
+            $category->delete();
+            return redirect()->route('kategori.index')->with('success', 'Kategori berhasil dihapus');
+        } catch (\Exception $e) {
+            return "{$e->getMessage()}, {$e->getCode()}";
+        }
     }
 }
--- resources/views/admin/type/actions.blade.php
+++ resources/views/admin/type/actions.blade.php
@@ -1,9 +1,11 @@
 <div class="d-flex">
-    <a href="#" class="btn btn-outline-dark btn-sm me-2"><i class="bi-person-lines-fill"></i></a>
-    <a href="#" class="btn btn-outline-dark btn-sm me-2"><i class="bi-pencil-square"></i></a>
+    <a href="{{ route('kategori.show', $type->id) }}" class="btn btn-outline-dark btn-sm me-2"><i
+            class="bi-person-lines-fill"></i></a>
+    <a href="{{ route('kategori.edit', $type->id) }}" class="btn btn-outline-dark btn-sm me-2"><i
+            class="bi-pencil-square"></i></a>
 
     <div>
-        <form action="#" method="POST">
+        <form action="{{ route('kategori.destroy', $type->id) }}" method="POST">
             @csrf
             @method('delete')
             <button type="submit" class="btn btn-outline-dark btn-sm me-2 btn-delete" data-name="#">
